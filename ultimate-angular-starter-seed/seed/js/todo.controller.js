a36fd886aa915d0094826b703993aab720d1fb7e
Micah Bales <micahbales@gmail.com>
2019-01-10 11:36:57 -0800

    ng-switch

--- ultimate-angular-starter-seed/seed/js/todo.controller.js
+++ ultimate-angular-starter-seed/seed/js/todo.controller.js
@@ -3,16 +3,25 @@ function TodoController() {
     title: 'First todo item',
     completed: true,
     rating: 5,
+    category: 'Personal',
   },
   {
     title: 'Number 2',
     completed: false,
     rating: 2,
+    category: 'Work',
   },
   {
     title: '#3 FTW!',
     completed: false,
     rating: 4,
+    category: 'Weekend',
+  },
+  {
+    title: 'And now for something completely different',
+    completed: false,
+    rating: 1,
+    category: 'My WeIrDo CaTeGoRy',
   }]
 }
 

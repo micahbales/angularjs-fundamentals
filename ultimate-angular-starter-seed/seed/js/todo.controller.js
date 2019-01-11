6db96f2ac89be663298ff5187176715d35874eee
Micah Bales <micahbales@gmail.com>
2019-01-11 13:34:36 -0800

    Lab 5 - Retrieve

--- ultimate-angular-starter-seed/seed/js/todo.controller.js
+++ ultimate-angular-starter-seed/seed/js/todo.controller.js
@@ -1,29 +1,15 @@
-function TodoController() {
-  this.newTodo = '';
-  this.list = [{
-    title: 'First todo item',
-    completed: true,
-    rating: 5,
-    category: 'Personal',
-  },
-  {
-    title: 'Number 2',
-    completed: false,
-    rating: 2,
-    category: 'Work',
-  },
-  {
-    title: '#3 FTW!',
-    completed: false,
-    rating: 4,
-    category: 'Weekend',
-  },
-  {
-    title: 'And now for something completely different',
-    completed: false,
-    rating: 1,
-    category: 'My WeIrDo CaTeGoRy',
-  }];
+function TodoController(TodoService) {
+  var ctrl = this;
+  ctrl.newTodo = '';
+  ctrl.list = [];
+
+  function getTodos() {
+    TodoService
+      .retrieve()
+      .then(function (response) {
+        ctrl.list = response;
+      })
+  }
 
   this.addTodo = function () {
     this.list.push({
@@ -58,6 +44,8 @@ function TodoController() {
   this.getCurrentTimestamp = function () {
     return new Date().getTime();
   }
+
+  getTodos();
 }
 
 angular
---
+++ ultimate-angular-starter-seed/seed/js/todo.service.js
@@ -0,0 +1,25 @@
+function TodoService($http) {
+
+  const API = 'https://jsonplaceholder.typicode.com/todos/';
+
+  function create() {}
+  function retrieve() {
+    return $http.get(API)
+      .then(function (response) {
+        return response.data.splice(0, 10);
+      });
+  }
+  function update() {}
+  function remove() {}
+
+  return {
+    create: create,
+    retrieve: retrieve,
+    update: update,
+    remove: remove,
+  }
+}
+
+angular
+  .module('app')
+  .factory('TodoService', TodoService);
